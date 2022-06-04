08a013149690ba4a3873e51ca271ab18ccbbb854
Jordan Ruocco <jpr34@students.uwf.edu>
2022-06-04 15:02:56 -0500

    minor changes

--- src/pages/playme.js
+++ src/pages/playme.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Board from '../TicTacToe/board';
-
-
 function TicTacToe(){
     return(
         <div style={{
@@ -13,18 +11,13 @@ function TicTacToe(){
             marginTop:'100px',
             background: 'rgba(0, 0, 0, 0.7)',
             padding: '30px',
-            
             paddingRight: '30px',
             fontSize: '28px',
             fontFamily: 'Gotham'
-            
-
-
         }}>
             <p><b>Play me in a game of Tic Tac Toe!</b></p>
             <Board />
         </div>
-        
     )
 }
 
