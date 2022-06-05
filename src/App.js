c729cefe4c19b98fd68f57a72fe59c5e009d9eaf
Jordan Ruocco <jpr34@students.uwf.edu>
2022-06-04 22:47:31 -0500

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import Experience from './pages/experience';
 import Skills from './pages/skills';
@@ -8,6 +8,7 @@ import TicTacToe from './pages/playme';
 import Education from './pages/education'
 import Projects from './pages/projects'
 import ButtonGroup from './ButtonGroup/ButtonGroup';
+import { useEffect, useState } from 'react';
 
 //Images
 import newyork from './images/newyork3.jpg';
@@ -15,14 +16,14 @@ import metlife from './images/metlife2.jpg';
 import japanmarket from './images/japanmarket2.jpg';
 import wave from './images/wave.png';
 import trains from './images/trains2.jpg';
-import { useEffect, useState } from 'react';
+
 function App() {
 
   const[backgroundImg, setBackgroundImg] = useState(newyork)
   useEffect(() => {
     const images = [newyork, metlife, wave, japanmarket, trains]
     setBackgroundImg(images[Math.floor(Math.random() * images.length)])
-  }) 
+  }, []) 
   document.body.style.backgroundImage = `url(${backgroundImg})`
   return (
     <>
